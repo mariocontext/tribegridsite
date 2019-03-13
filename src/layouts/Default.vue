034f37a5e1b850280ab0b0ffa317f84645ac83f2
Mario Noble <marionoble@Marios-MacBook-Air.local>
2019-03-12 20:08:42 -0700

    added initial pages and nav

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="layout">
     <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
-      </strong>
+      <span>Tribe Talent</span>
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
+        <ul>
+          <li>
+            <g-link class="nav__link" to="/">Home</g-link></li>
+          <li>
+            <g-link class="nav__link" to="/about">About</g-link>
+          </li>
+          <li>
+            <g-link class="nav__link" to="/hire">Hire</g-link>
+          </li>
+          <li>
+            <g-link class="nav__link" to="/join">Join</g-link>
+          </li>
+        </ul>
       </nav>
     </header>
     <slot/>
@@ -22,29 +31,4 @@ query {
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,17 +2,11 @@
   <Layout>
     
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
     
-    <h1 class="tw-text-green tw-pb-12 my-special-class tw-bg-orange">Hello, world!</h1>
+    <h1>Hello, world!</h1>
    
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
 
   </Layout>
@@ -21,7 +15,7 @@
 <script>
 export default {
   metaInfo: {
-    title: "Hello, world!"
+    title: "Welcome"
   }
 };
 </script>
