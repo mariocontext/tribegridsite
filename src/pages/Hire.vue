40f0605a313db427074c1ebce12691afde858b64
Mario Noble <marionoble@Marios-MacBook-Air.local>
2019-03-13 11:15:18 -0700

    added initial content for Index and Hire plus various components

--- src/pages/Hire.vue
+++ src/pages/Hire.vue
@@ -1,14 +1,104 @@
 <template>
   <Layout>
-    <h1>Hire</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <section>
+      <h1>Making hiring easy</h1>
+      <p>Tribe makes hiring your next designer + developer fast, easy and transparent! Learn More Today</p>
+    </section>
+
+    <section>
+      <ul>
+        <li><a href="#">Email us</a></li>
+        <li><a href="#">Call us</a></li>
+        <li><a href="#">Let's grab coffee</a></li>
+        <li><a href="#">Text us</a></li>
+      </ul>
+    </section>
+
+    <section>
+      <h2>Get in touch</h2>
+      <p>Tribe makes hiring your next designer + developer fast, easy and transparent! Learn More Today</p>
+
+      <div>
+        <form id="email-hire" name="email-hire" data-name="Email Hire">
+          <div>
+          <label for="hire-name">Name</label>
+          <input type="text" maxlength="256" name="hire-name" data-name="Hire-Name" placeholder="Jane Jones" id="hire-name" required="">
+          </div>
+          <div>
+          <label for="hire-email">Email</label>
+          <input type="email" maxlength="256" name="Hire-Email" data-name="Email" placeholder="jane@tribemail.com" id="Email" required="">
+          </div>
+
+          <div>
+          <label for="hire-phone">Phone</label>
+          <input type="tel" maxlength="256" name="hire-phone" data-name="Hire Phone" placeholder="1-202-123-1234" id="phone" required="">
+          </div>
+
+          <div>
+          <label for="hire-industry">Industry</label>
+          <input type="text" maxlength="256" name="hire-industry" data-name="Hire Industry" id="hire-industry" required="">
+          </div>
+
+          <div>
+          <label for="hire-location">Location</label>
+          <input type="text" class="w-input" maxlength="256" name="hire-location" data-name="Hire Location" id="hire-location" required="">
+          </div>
+          
+          <div>
+          <label for="hire-team-size">Your Team Size</label>
+          <select id="hire-team-size" name="hire-team-size" data-name="Hire Team Size">
+            <option value="10 - 20">10 - 20</option>
+            <option value="20 - 50">20 - 50</option>
+            <option value="50 - 100">50 - 100</option>
+            <option value="100 +">100 +</option>
+          </select>
+          </div>
+          
+          <div>
+
+              <h4>Would you prefer call or email?</h4>
+
+              <input type="radio" id="hire-call-pref" name="hire-call-pref" value="call" data-name="Hire Call">
+              <label for="hire-call-pref">Call</label>
+
+              <input type="radio" id="hire-email-pref" name="Hire Email Pref" value="Email" data-name="hire-email-pref">
+              <label for="hire-email-pref">Email</label>
+              
+            </div>
+
+            <input type="submit" value="Get in Touch">
+
+        </form>
+      </div>
+    </section>
+
+    <BrandLove />
+
+    <HireBenefits />
+
+    <section>
+      <h2>Unsure about tribe?</h2>
+      <a href="#">Let's grab coffee</a>
+    </section>
+
+    <OurBlog />
+    
   </Layout>
 </template>
 
 <script>
+import HireBenefits from "~/components/HireBenefits.vue";
+import BrandLove from "~/components/BrandLove.vue";
+import OurBlog from "~/components/OurBlog.vue";
+
 export default {
   metaInfo: {
     title: "Hire"
+  },
+  components: {
+    HireBenefits,
+    BrandLove,
+    OurBlog
   }
 };
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,61 +4,80 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
 
     <main>
-    <section>
-      <h1>Making Hiring Easy</h1>
-      <p>Tribe makes hiring your next designer + developer fast, easy and transparent! Learn More Today</p>
-    </section>
-    <section>
-      <h2>How Tribe is Making Hiring Easy</h2>
-        <section>
-          <h3>Post a Job for Free</h3>
-          <p>Work with our engineers to get all the placement details right. We look for more that just keywords when placing our members.</p>
-        </section>
-        <section>
-          <h3>Connect With Members</h3>
-          <p>Whether you need a team or individual contributor. Discuss placement details with pre-vetted, interested members directly on the platform. No more email back and forth.</p>
-        </section>
-        <section>
-          <h3>Simplify Payments</h3>
-          <p>Only pay for approved hours through debit, credit, or ACH deposits. We handle all the payroll, compliance, and benefits for our members.</p>
-        </section>
-        <section>
-          <h3>Interview on the Platform</h3>
-          <p>Save time and money. Sync your calendar and schedule in-person or video interviews directly from the platform.</p>
-        </section>
-        <section>
-          <h3>Track Time and Deliverables</h3>
-          <p>Easily keep up to date with weekly timecards, deliverables, and deadlines directly from the platform. </p>
-        </section>
-    </section>
 
-    <section>
-      <h2>Brands love us!</h2>
-      <ul>
-        <li>Colab</li>
-        <li>LogMeIn</li>
-        <li>Bullhorn</li>
-        <li>Arena</li>
-      </ul>
-    </section>
-
-    <section>
-      <h2>Unsure About Tribe?</h2>
-      <a href="#">Let's grab coffee</a>
-    </section>
-
-    <aside>
-      <h2>Our Blog</h2>
       <section>
-        <h3>Learn the top 5 reasons why to join a worker cooperative!</h3>
-        <a href="#">Learn More About Cooperatives</a>
+        <h1>Hiring is Hard</h1>
+        <p>Tribe makes Hiring Easier by directly connecting companies to tech+creative talent</p>
+        <a href="#">Make it Easy</a>
       </section>
+
       <section>
-        <h3>How Tribe is helping Tech + Creative professionals earn more ?</h3>
-        <a href="#">Discover Tribes Story + Mission</a>
+        <h1>Freelancing is harder</h1>
+        <p>Tribe helps tech+creative professionals find consistent work and pay by joining a community of highly motivated professionals.</p>
+        <a href="#">Earn More</a>
       </section>
-      
-    </aside>
+
+      <section>
+        <h2>Find great talent</h2>
+        <p>Tribe is the only worker owned staffing platform. Find local and vetted talent for your team without the overhead.</p>
+
+        <div class="mini-bio">
+          <h3>Name</h3>
+          <span>City, ST</span>
+          <span>Title</span>
+          <div>
+            <ul>
+              <li>Skill</li>
+              <li>Skill</li>
+            </ul>
+            <p>image here?</p>
+            <a href="#">Schedule a call</a>
+          </div>
+        </div>
+
+        <div>
+          <h2>Find great jobs</h2>
+          <p>Tribe is the only worker owned staffing platform. Find local and remote vetted talent for your team without the overhead.</p>
+          <ul>
+            <li>Skill</li>
+            <li>Skill</li>
+            <li>Skill</li>
+            <li>Skill</li>
+            <li>Skill</li>
+            <li>Skill</li>
+            <li>Skill</li>
+            <li>Skill</li>
+          </ul>
+          <a href="#">Find more jobs</a>
+        </div>
+
+      </section>
+
+      <section>
+        <h2>How it works</h2>
+        <div>
+          Video goes here
+        </div>
+
+        <HireBenefits />
+
+      </section>
+
+      <section>
+        <span>Testimonial Logo</span>
+        <p>Testimonial text</p>
+        <span>Org Logo</span>
+        <span>Org Person image</span>
+      </section>
+
+      <section>
+        <h2>Questions?</h2>
+        <p>Schedule a call to get started</p>
+      </section>
+
+      <BrandLove />
+
+      <OurBlog />
 
     </main>
 
@@ -66,9 +85,18 @@
 </template>
 
 <script>
+import HireBenefits from "~/components/HireBenefits.vue";
+import BrandLove from "~/components/BrandLove.vue";
+import OurBlog from "~/components/OurBlog.vue";
+
 export default {
   metaInfo: {
     title: "Welcome"
+  },
+  components: {
+    HireBenefits,
+    BrandLove,
+    OurBlog
   }
 };
 </script>
