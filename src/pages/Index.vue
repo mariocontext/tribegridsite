f2c96ecd60324cc4fa9f12122c380c138113a836
Mario Noble <marionoble@Marios-MacBook-Air.local>
2019-03-12 20:34:57 -0700

    added initial index content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,12 +2,65 @@
   <Layout>
     
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    
-    <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+
+    <main>
+    <section>
+      <h1>Making Hiring Easy</h1>
+      <p>Tribe makes hiring your next designer + developer fast, easy and transparent! Learn More Today</p>
+    </section>
+    <section>
+      <h2>How Tribe is Making Hiring Easy</h2>
+        <section>
+          <h3>Post a Job for Free</h3>
+          <p>Work with our engineers to get all the placement details right. We look for more that just keywords when placing our members.</p>
+        </section>
+        <section>
+          <h3>Connect With Members</h3>
+          <p>Whether you need a team or individual contributor. Discuss placement details with pre-vetted, interested members directly on the platform. No more email back and forth.</p>
+        </section>
+        <section>
+          <h3>Simplify Payments</h3>
+          <p>Only pay for approved hours through debit, credit, or ACH deposits. We handle all the payroll, compliance, and benefits for our members.</p>
+        </section>
+        <section>
+          <h3>Interview on the Platform</h3>
+          <p>Save time and money. Sync your calendar and schedule in-person or video interviews directly from the platform.</p>
+        </section>
+        <section>
+          <h3>Track Time and Deliverables</h3>
+          <p>Easily keep up to date with weekly timecards, deliverables, and deadlines directly from the platform. </p>
+        </section>
+    </section>
+
+    <section>
+      <h2>Brands love us!</h2>
+      <ul>
+        <li>Colab</li>
+        <li>LogMeIn</li>
+        <li>Bullhorn</li>
+        <li>Arena</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2>Unsure About Tribe?</h2>
+      <a href="#">Let's grab coffee</a>
+    </section>
+
+    <aside>
+      <h2>Our Blog</h2>
+      <section>
+        <h3>Learn the top 5 reasons why to join a worker cooperative!</h3>
+        <a href="#">Learn More About Cooperatives</a>
+      </section>
+      <section>
+        <h3>How Tribe is helping Tech + Creative professionals earn more ?</h3>
+        <a href="#">Discover Tribes Story + Mission</a>
+      </section>
+      
+    </aside>
+
+    </main>
 
   </Layout>
 </template>
